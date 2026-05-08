0f1b5301b8739521d89d688cb3b9056f8d61f683
Ozsa01 <onur.sahin2012@gmail.com>
2026-05-08 15:26:52 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:40}}>
+      <h1>SANY & FMT</h1>
+      <p>İnsan Kaynakları Sistemi</p>
+      <a href="/login">Giriş Yap</a>
+    </div>
+  )
+}
